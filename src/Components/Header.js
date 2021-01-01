cf76ed0c0afaf72f080fd45fb9f711cf614d0e92
Anthony Chen <achen@novacoast.com>
2021-01-01 19:33:57 +0000

    boilerplate for routing and new pages

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,18 +2,28 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from 'react-bootstrap/Navbar'
 import {Nav} from 'react-bootstrap';
+import {Switch, Route} from 'react-router-dom';
+import Home from "../Pages/Home"
+import Page1 from "../Pages/Page1"
 
 function Header() {
     //Header
     return (
-        <Navbar bg="light" variant="light">
-            <Navbar.Brand href="#home">App Name</Navbar.Brand>
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav.Link href="#Page1" className="text-muted">Page 1</Nav.Link>
-                <Nav.Link href="#Page2" className="text-muted">Page 2</Nav.Link>
-                <Nav.Link href="#Page3" className="text-muted">Page 3</Nav.Link>
-            </Navbar.Collapse>
-        </Navbar>
+        <div>
+            <Navbar bg="light" variant="light">
+                <Navbar.Brand href="/">App Name</Navbar.Brand>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav.Link href="/Page1" className="text-muted">Page 1</Nav.Link>
+                    <Nav.Link href="Page2" className="text-muted">Page 2</Nav.Link>
+                    <Nav.Link href="Page3" className="text-muted">Page 3</Nav.Link>
+                </Navbar.Collapse>
+            </Navbar>
+            <Switch>
+                <Route exact path='/' component={Home}/>
+                <Route path='/Page1' component={Page1}/>
+            </Switch>
+        </div>
+        
     );
 }
 
