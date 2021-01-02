d60ce3abb397cd44604565dae530b71bb83936f2
Anthony Chen <tomokoNALS@gmail.com>
2021-01-02 23:24:45 +0000

    reorganized the routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import Header from './Components/Header'
+import {Switch, Route} from 'react-router-dom';
+// import Header from './Components/Header'
+import Home from './Pages/Home/Home';
+import About from "./Pages/About";
+import Locate from "./Pages/Locate/Locate";
 // import Footer from './Components/Footer'
 
 function App() {
   return (
     <div> 
-      <Header/>
-      {/* <Footer/> */}
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route path='/About' component={About}/>
+        <Route path='/Locate' component={Locate}/>
+      </Switch>
     </div>
   );
 }
