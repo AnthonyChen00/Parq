fdd501017608ae22fe27a9446d6077dbbd5f189b
Anthony Chen <tomokoNALS@gmail.com>
2021-01-02 01:38:58 +0000

    Rough implementation of Search bar

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import homephoto from '../../Assets/homephoto.jpg'
-import './Home.css'
-import Image from 'react-bootstrap/Image'
+import './Home.css';
+import Image from 'react-bootstrap/Image';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 function Home(){
     return(
-        <div className="container-fluid imagebox">
-            <Image src={homephoto} fluid/>
-            <div className="title">
-                <h1>Parq</h1>
-                <h2 className="">Parking made easy</h2>
+        <div>
+            <div className="container-fluid imagebox">
+                <Image src={homephoto} fluid/>
+                <div className="title">
+                    <h1>Parq</h1>
+                    <h2 className="">Parking made easy</h2>
+                </div>
             </div>
+            <form>
+                <Form.Group controlId="address">
+                    <Form.Label>Address</Form.Label>
+                    <Form.Control type="address" placeholder="Enter address"/>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </form>
         </div>
     )
 }
