e760297e08fc2b961439c56a437b05a39c8bd394
Anthony Chen <achen@novacoast.com>
2021-01-03 04:54:00 +0000

    Reworked the height of the list

--- src/Pages/Locate/Locate.js
+++ src/Pages/Locate/Locate.js
@@ -5,21 +5,34 @@ import "./Locate.css"
 
 function Locate(props){
     return(
+        // <div>
+        //     {/* Header will contain information - address - search for other address */}
+        //     <div className="container-fluid pl-0 view">
+        //         <div className="row">
+        //             <div classname="col-xs">
+        //                 <MapContainer/>
+        //             </div>
+        //             <div className="col-xs">
+        //                 <h5>Parking Spots Nearby</h5>
+        //                 <SearchResults/>
+        //             </div>
+        //         </div>
+
+        //     </div>
+        // </div>
         <div>
-            {/* Header will contain information - address - search for other address */}
             <LocateHeader address={props.location.state.address}/>
-            <div className="container-fluid pl-0 view">
-                <div className="row">
-                    <div classname="col-xs-6">
+            <div className="container-fluid">
+                <div className="row view">
+                    <div className="col-9 pl-0">
                         <MapContainer/>
                     </div>
-                    <div className="card col-md-auto p-3 mt-4">
-                        <h5>Parking Spots Nearby</h5>
+                    <div className="col-3 list">
                         <SearchResults/>
                     </div>
                 </div>
-
             </div>
+            
         </div>
     )
 }
